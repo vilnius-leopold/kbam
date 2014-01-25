b2d5a67d88ded806d77ae701212a34941073b832
Leopold Burdyl <nerd@whiteslash.eu>
2014-01-25 18:56:39 +0200

    Move rspec settings from spec_helper to .rspec file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,7 @@
 require_relative '../lib/kbam.rb'
 require_relative 'create_schema.rb'
 
-RSpec.configure do |config|
-  # Use color in STDOUT
-  config.color_enabled = true
-
-  # Use color not only in STDOUT but also in pagers and files
-  config.tty = true
-
-  # Use the specified formatter
-  config.formatter = :documentation # :progress, :html, :textmate
-end
-
+# turn of kbam verbose query output
 Kbam.verbose false
 
 # initial setup of the database
