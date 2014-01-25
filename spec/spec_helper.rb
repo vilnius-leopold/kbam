72a1ade49f672ece8f11dcb3792a1fc464b87643
Leopold Burdyl <nerd@whiteslash.eu>
2014-01-25 18:08:38 +0200

    Add colored rspec prompt. Call database setup function explicitly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,18 @@
 require_relative '../lib/kbam.rb'
 require_relative 'create_schema.rb'
 
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
+
+Kbam.verbose false
+
+# initial setup of the database
+setup_database
